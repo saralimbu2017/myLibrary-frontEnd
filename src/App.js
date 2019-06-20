fbd73a2042ae5de12a2831927b3571efcc50e01d
sara limbu <limbusaraswati@gmail.com>
2019-06-20 23:00:31 +1000

    Sending GGet request from react form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form'
+import Box from './Box'
+import { withRouter } from "react-router";
 
 class App extends React.Component {
   state= {
     books:[
-    ]
+    ],
+    bookName:''
   }
 
-  componentDidMount(){
-  
+  submitForm = (event) => { 
+    event.preventDefault();
+    const searchItem = this.state.bookName
+    const url = `http://localhost:8080/books?bookName=${searchItem}`
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          books: res
+        })
+      })
+  }
+  handleFormChange = (event) => {
+    this.setState({ bookName: event.target.value })
   }
   render() {
     return (
       <div className="App">
-        <div class = "ui-container"> 
-          <p>hello</p>
+        <div className = "ui-container"> 
+          <Form handleSubmit = {this.submitForm} value ={this.state.bookName} handleChange={this.handleFormchange}/>     
         </div>
       </div>
     );
   }
 }
+
+
+
 
 export default App;
 
