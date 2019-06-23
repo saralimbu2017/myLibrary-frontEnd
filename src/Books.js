7fb0561cec3825946a9363ef3de6d7008f9f2cea
sara limbu <limbusaraswati@gmail.com>
2019-06-24 04:18:09 +1000

    UI for the Books component

--- src/Books.js
+++ src/Books.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import Search from './Search'
+import './Books.css';
 
 export default function Books(props) {
-  //render() {
+  function sliceBooks(array, size) {
+    const bookCollection = [];
+    let index = 0;
+    while (index < array.length) {
+      bookCollection .push(array.slice(index, size + index));
+      index += size;
+    }
+    return bookCollection;
+  }
+  const bookCollection = sliceBooks(props.location.state.books,4)
+  console.log(bookCollection)
   return (
     <div className = "ui-main-wrapper">
       <div className = "ui-main">
-        <ul>
-          {props.location.state.books.map(book =>
-            <li>
-              {book.volumeInfo.title}
-            </li>
+        <div className = "ui-bestSellerBooks"> 
+          { bookCollection.map(books =>
+             <div>
+              <div className = "ui-box">
+                {books.map(book =>
+                  <img  key = "book.title" src = {book.volumeInfo.imageLinks.smallThumbnail}/>
+                )}
+              </div>
+              <div className = "ui-rack">
+
+                  </div>
+              </div>
           )}
-        </ul>
+        </div> 
       </div>
       <div className = "ui-search">
         <Search />
