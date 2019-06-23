733df0f62dcb8c5257bc7e477a017e4e1968cf48
sara limbu <limbusaraswati@gmail.com>
2019-06-23 16:09:11 +1000

    Setting up the routes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Form from './Form'
-import Box from './Box'
-import { withRouter } from "react-router";
-
+import {Switch , Link, Route,BrowserRouter as Router, Redirect} from 'react-router-dom';
+import About from './About'
+import Home from './Home'
 class App extends React.Component {
-  state= {
-    books:[
-    ],
-    bookName:''
-  }
 
-  submitForm = (event) => { 
-    event.preventDefault();
-    const searchItem = this.state.bookName
-    const url = `http://localhost:8080/books?bookName=${searchItem}`
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          books: res
-        })
-      })
-  }
-  handleFormChange = (event) => {
-    this.setState({ bookName: event.target.value })
-  }
+
+
   render() {
+  
     return (
-      <div className="App">
-        <div className = "ui-container"> 
-          <Form handleSubmit = {this.submitForm} value ={this.state.bookName} handleChange={this.handleFormchange}/>     
+      <Router>
+        <div className="App">
+          <div className = "ui-container">
+            <Switch>
+              <Route exact path="/" component = {Home} />
+              <Route path="/about" component = {About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>  
     );
   }
 }
-
-
-
 
 export default App;
 
--- src/Box.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function Box(props) {
-  return <li>
-    {props.book.volumeInfo.title}
-  </li>
-
-}
